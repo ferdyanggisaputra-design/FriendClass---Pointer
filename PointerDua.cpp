a12cc3fe771560da51ca7bb4152341fab4d68d1b
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-27 12:12:14 +0700

    Menambah implementasi fungsi main dan atribut nim pada kelas mahasiswa

--- PointerDua.cpp
+++ PointerDua.cpp
@@ -3,20 +3,3 @@ using namespace std;
 
 class mahasiswa
 {
-public:
-    int nim;
-
-    void showNim()
-    {
-        cout << "No Induk = " << nim << endl;
-    }
-};
-
-int main()
-{
-    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
-    mhs->nim = 2;
-    mhs->showNim();
-    delete mhs;
-    return 0;
-}
