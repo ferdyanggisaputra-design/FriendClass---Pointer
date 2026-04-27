3046fdcf70af39eb12f8d57d48848ad95115ce15
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-27 12:11:04 +0700

    Menambahkan implementasi pointer dereference dan arrow operator pada fungsi main

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -18,4 +18,9 @@ int main(){
     refMhs.nim = 2;          // Member Access Operator
     mhs.showNim();
 
-    
+    mahasiswa *pMhs = &mhs;  // pointer Dereference pMhs
+    pMhs->nim = 3;           // Arrow Operator
+    pMhs->showNim();
+
+    return 0;
+}
